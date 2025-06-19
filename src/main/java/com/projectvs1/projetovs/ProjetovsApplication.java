38e942376e1bfb381986f98db620a7228468482d
Felipe Valboeno <feliperocha14@hotmail.com>
2025-06-19 09:48:38 -0300

    Projeto criado - implementando Produto/Model

---
+++ src/main/java/com/projectvs1/projetovs/ProjetovsApplication.java
@@ -0,0 +1,15 @@
+package com.projectvs1.projetovs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetovsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetovsApplication.class, args);
+
+		
+	}
+
+}
---
+++ src/test/java/com/projectvs1/projetovs/ProjetovsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectvs1.projetovs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetovsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
